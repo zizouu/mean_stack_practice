08f19f92bb7ab6d983fa1f1a1dea4a51900ee44b
HanJisoo <thecarlos@naver.com>
2017-11-03 16:49:18 +0900

    express server structure setting

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Get dependencies
 const express = require('express');
 const path = require('path');
-const http = require('http');
 const bodyParser = require('body-parser');
 
 // Get our API routes
@@ -9,33 +8,14 @@ const api = require('./server/routes/api');
 
 const app = express();
 
-// Parsers for POST data
+// Set app config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
 app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(function (err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('server error...');
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+module.exports = app;
--- server/routes/api.js
+++ server/routes/api.js
@@ -5,4 +5,20 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+router.get('/lotto', (req, res) => {
+  res.download('d:\\', '/dist/index.html');
+});
+
 module.exports = router;
+
+router.get('/stock/list/all', (req, res) => {
+  const pgp = require('pg-promise')({});
+  const db = pgp('postgres://stock_admin:!Han382612@54.190.58.114:5432/stock');
+  db.many('SELECT * FROM stock_item')
+    .then(function (data) {
+      res.send(data);
+    })
+    .catch(function(error){
+      console.log(error.stack);
+    });
+});
